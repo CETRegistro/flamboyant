abc0ca86229ad02965b53ae2a436c17af8fd97a2
PortfolioJovePy <165056801+PortfolioJovePy@users.noreply.github.com>
2025-07-15 11:58:38 -0300

    enviar pra deploy

--- api/urls.py
+++ api/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('flamboyant.urls')),
+    
     path('lavanderia/', include('lavanderia.urls')),
 ]
